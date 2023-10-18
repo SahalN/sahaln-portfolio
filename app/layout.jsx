1a5372dd4153d7554646def4202351488ce90992
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-18 22:12:54 +0700

    add layout and navigating

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,32 @@
 /** @format */
 
+import Link from "next/link";
+
 export default function RootLayout({ children }) {
   return (
     <>
       <html lang='en'>
         <body>
-          <header>[]</header>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <a href='/'>Home</a>
+                </li>
+                <li>
+                  <Link href='/works'>Works</Link>
+                </li>
+                <li>
+                  <Link href='/blogs' prefetch={false}>
+                    Blogs
+                  </Link>
+                </li>
+                <li>
+                  <Link href='/github'>Github</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
           <main>{children}</main>
           <footer>[]</footer>
         </body>
