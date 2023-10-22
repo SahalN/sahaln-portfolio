b42f1bfc82c0a3b2dd28c95e6048431d62d333a4
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-22 08:10:05 +0700

    Add header components and import alias

--- app/github/page.jsx
+++ app/github/page.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import Header from "../../components/Header";
+import Header from "@/components/Header";
 
 export default function GithubPage() {
   return <Header>Hello This is github page</Header>;
