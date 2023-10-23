34ea740d8a53a7a7b8bfe287e867e97d0d4e3e73
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-10-23 10:21:29 +0700

    add npm marked

--- app/works/gameverse-chronicles/page.jsx
+++ app/works/gameverse-chronicles/page.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import Heading from "@/components/Heading";
+import { marked } from "marked";
 import { readFile } from "node:fs/promises";
 
 export default async function GameverseChronicles() {
@@ -8,6 +9,7 @@ export default async function GameverseChronicles() {
     "./content/reviews/gameverse-chronicles.md",
     "utf8"
   );
+  const html = marked(text);
   return (
     <>
       <Heading>Gameverse Chronicles</Heading>
